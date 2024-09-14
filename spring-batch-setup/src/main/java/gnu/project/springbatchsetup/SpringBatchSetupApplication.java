b97f58b1207ef33f11df82b2186e07baa593a552
Shinjan Khastagir <37021806+ShakeYourLittleBrain@users.noreply.github.com>
2024-09-14 19:59:20 +0200

    spring batch project

---
+++ spring-batch-setup/src/main/java/gnu/project/springbatchsetup/SpringBatchSetupApplication.java
@@ -0,0 +1,13 @@
+package gnu.project.springbatchsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchSetupApplication.class, args);
+    }
+
+}
---
+++ spring-batch-setup/src/test/java/gnu/project/springbatchsetup/SpringBatchSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package gnu.project.springbatchsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
